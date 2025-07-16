8c7f23831abca78c2727cd5cb67365d507b874a1
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2025-07-16 05:42:42 +0500

    feat: add MarkdownFancyLists plugin

--- app/src/markdownit.ts
+++ app/src/markdownit.ts
@@ -7,6 +7,7 @@ import { full as MarkdownItEmoji } from 'https://esm.sh/markdown-it-emoji@3.0.0'
 import { default as MarkdownItFootnote } from 'https://esm.sh/markdown-it-footnote@4.0.0';
 import { default as MarkdownItTaskLists } from 'https://esm.sh/markdown-it-task-lists@2.1.1';
 import { default as MarkdownItTexmath } from 'https://esm.sh/markdown-it-texmath@1.0.0';
+import { markdownItFancyListPlugin as MarkdownItFancyLists } from 'https://esm.sh/markdown-it-fancy-list@1.3.0';
 import { default as MarkdownItGitHubAlerts } from 'https://esm.sh/markdown-it-github-alerts@1.0.0';
 import Katex from 'https://esm.sh/katex@0.16.9';
 
@@ -33,6 +34,7 @@ const md = new MarkdownIt('default', {
     }),
 })
   .use(MarkdownItEmoji)
+  .use(MarkdownItFancyLists)
   .use(MarkdownItFootnote)
   .use(MarkdownItTaskLists, { enabled: false, label: true })
   .use(MarkdownItTexmath, {
