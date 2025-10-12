7ab3e37514c2b8a95bd835e23712b1d245349d4a
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2025-10-13 02:06:05 +0500

    chore: Update deps

--- client/src/script.ts
+++ client/src/script.ts
@@ -1,7 +1,7 @@
 import { debounce, findLast, getInjectConfig } from './util.ts';
 import { slidingWindows } from 'https://deno.land/std@0.224.0/collections/sliding_windows.ts';
 // @deno-types="https://raw.githubusercontent.com/patrick-steele-idem/morphdom/master/index.d.ts"
-import morphdom from 'https://esm.sh/morphdom@2.7.2?no-dts';
+import morphdom from 'https://esm.sh/morphdom@2.7.7?no-dts';
 import mermaid from './mermaid.ts';
 
 const window = globalThis;
